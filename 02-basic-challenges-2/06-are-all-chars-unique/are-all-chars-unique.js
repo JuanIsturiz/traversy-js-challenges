128bc3ab478df333fb75f25738b110d75fc7c59a
JuanIsturiz <juanandresisturiz@gmail.com>
2024-01-20 13:06:53 -0400

    COMPLETED 02-basic-challenges 06-07

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,16 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const dict = str
+    .split("")
+    .reduce(
+      (acc, el) =>
+        el in acc ? { ...acc, [el]: acc[el] + 1 } : { ...acc, [el]: 1 },
+      {}
+    );
+
+  for (const key in dict) {
+    if (dict[key] > 1) return false;
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,16 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const dict = str
+    .split("")
+    .reduce(
+      (acc, el) =>
+        el in acc ? { ...acc, [el]: acc[el] + 1 } : { ...acc, [el]: 1 },
+      {}
+    );
+
+  for (const key in dict) {
+    if (dict[key] === 1) return key;
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
